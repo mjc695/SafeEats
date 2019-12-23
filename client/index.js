d65a10c7af200dcaa7aec63bdeab071584195146
Monferd Collin <monferd.j.collin@gmail.com>
2019-12-23 14:22:45 -0500

    added front end for client

---
+++ client/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './store'
+import history from './history'
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={history}>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('app')
+)
